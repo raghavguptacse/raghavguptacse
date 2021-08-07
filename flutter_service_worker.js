c4d0ed31a523ee6612dadbd21893166cf988280b
Raghav Gupta <E19CSE258@bennett.edu.in>
2021-08-08 02:31:49 +0530

    V1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,16 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "f1d3afe2c2581616447efd909b11aa10",
+  "assets/AssetManifest.json": "6a2d992ab4b4a51a8fd83f4f64eb68a7",
 "assets/Assets/Fonts/Nunito/Nunito-Bold.ttf": "c0844c990ecaaeb9f124758d38df4f3f",
 "assets/Assets/Images/Algo.png": "cebd5aafecbf06f90f5c423d5a2f3972",
 "assets/Assets/Images/computer.png": "da9af4c4611c58a008734d0a2b7a045a",
+"assets/Assets/Images/consult.png": "ca57d5e21892a7783a36171e01db1974",
 "assets/Assets/Images/cryptography.png": "7055ab3a2c5a76478d16d7cc91bae24a",
+"assets/Assets/Images/design.png": "0f88e3a8e59c9127511b6081b05551f6",
 "assets/Assets/Images/graph.png": "d19de052d638db6452945eb56c8843ef",
 "assets/Assets/Images/person.png": "5cde9490370ac55b8033a2aadbc16561",
 "assets/Assets/Images/prof.jpg": "ed5fc8b56dd0539082144fd0f07aefb3",
 "assets/Assets/Images/quantum-computing.png": "82403f35d26e1b05b36ec1ab366cf77a",
 "assets/Assets/Images/quantum.png": "9144e200d3403b16a0aec1badf8588bb",
+"assets/Assets/Images/research.png": "f2d0c7f2ddff10b71983e7a95b4d44f5",
 "assets/Assets/Images/solutions.png": "23101aba01f1f691fd03b0b0880a601a",
 "assets/Assets/Images/writing.png": "e5609fcf3a216e23d7cc1c825de2478e",
 "assets/FontManifest.json": "a718e1a51bf16a5ced2d7a1b19894584",
@@ -22,11 +25,10 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3717f58513fc7e664e05b4483db7a427",
-"/": "3717f58513fc7e664e05b4483db7a427",
-"main.dart.js": "8380f607690c8d3e7f1ce37887fdcc8b",
+"index.html": "a74fc474dd1d5e1d2ced483ee1b74746",
+"/": "a74fc474dd1d5e1d2ced483ee1b74746",
+"main.dart.js": "1d831182005d6f3e323e79b5d7f2bce9",
 "manifest.json": "5eea30b2f5debb2d2332f4e703fba412",
-"q.zip": "61e9b6cb32729c848e2ec8cd165e1e25",
 "version.json": "52ec77be557e5fb433af14c55bedcc04"
 };
 
